2ffadea2b2104d39347b8c73a8ea8306d9842e99
Maycon J Pereira <mayconjovan@gmail.com>
2025-01-26 19:30:30 -0300

    create payment-validator project

---
+++ pen-payment-validator/src/main/java/com/mjp/pen_payment_validator/PenPaymentValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.mjp.pen_payment_validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenPaymentValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenPaymentValidatorApplication.class, args);
+	}
+
+}
---
+++ pen-payment-validator/src/test/java/com/mjp/pen_payment_validator/PenPaymentValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjp.pen_payment_validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenPaymentValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
